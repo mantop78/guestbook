5d44b5748af88e0e685a25961c0eeb83d918afa2
mantop78 <mantop78@gmail.com>
2020-03-23 15:58:50 +0100

    Twig aggiunte cose

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,37 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+#use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="homepage")
+     * @Route("/", name="homepage")
      */
 
-    #public function index(Request $request)
-    public function index(string $name  = '')
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {   
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>',htmlspecialchars($name));
-        }
-        #if ($name = $request->query->get('hello')) {
-        #    $greet = sprintf('<h1>Hello %s!</h1>',htmlspecialchars($name));
-        #}
+        return new Response($twig->render('conference/index.html.twig', ['conferences' => $conferenceRepository->findALL(),]));
+    }
 
-        return new Response(<<<EOF
-        
-        <html>
-            <body>
-                $greet
-                <img src="/images/under-construction.gif" />
-            </body>
-        </html>
-        EOF
-        );
+
+/**
+* @Route("/conference/{id}", name="conference")
+*/
+    public function show(Environment $twig, Conference $conference,CommentRepository $commentRepository)
+    {
+        return new Response($twig->render('conference/show.html.twig', ['conference' => $conference,
+        'comments' => $commentRepository->findBy(['conference' =>$conference], ['createdAt' => 'DESC'])]));
     }
 }
